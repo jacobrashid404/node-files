8c1f64099a01ec2217d76479b2731ad0b983e2cb
Jacob <jacobrashid404@gmail.com>
2024-05-14 13:10:24 -0700

    cat function

---
+++ step1.js
@@ -0,0 +1,17 @@
+import * as fs from "fs/promises"
+
+async function cat(path){
+  try {
+    let content = await fs.readFile(path, "utf8");
+    console.log("file content: ", content);
+  }
+  catch(err){
+    console.log("File not found");
+    process.exit(1);
+  }
+}
+
+const argv = process.argv;
+
+await cat(argv[2]);
+export { cat };
